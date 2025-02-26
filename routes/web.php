aa73ef79d2e389d418af9d9f1c462151b5b2ad2e
callmenosaint <otakuovn@gmail.com>
2025-02-27 03:21:30 +0700

    them trang chu cho client va admin

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ use Inertia\Inertia;
 //     ]);
 // });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('Dashboard');
+//     })->name('dashboard');
+// });
